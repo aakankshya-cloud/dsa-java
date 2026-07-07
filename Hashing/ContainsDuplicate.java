e78b1b1e63eb3bb0288702bbe19ff9ae5b9c7957
aakankshya-cloud <aakankshyadash411@gmail.com>
2026-07-07 22:56:17 +0530

    QS

--- Hashing/ContainsDuplicate.java
+++ Hashing/ContainsDuplicate.java
@@ -1,22 +1,22 @@
-import java.util.HashMap;
-
-public class ContainsDuplicate {
-    public boolean containsDuplicate(int[] nums){
-        HashMap<Integer,Integer> map = new HashMap<>();
-        for(int i = 0; i < nums.length; i++){
-            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
-        }
-        for(int value : map.values()){
-            if(value != 1) return false;
-        }
-        return true;
-//--------------------------------------------------------------------------------------------------
-        for(int i = 0; i < nums.length; i++){
-            if(map.containsKey(nums[i])){
-                return false;
-            }
-            map.put(nums[i],i);
-        }
-        return true;
-    }
-}
+//import java.util.HashMap;
+//
+//public class ContainsDuplicate {
+//    public boolean containsDuplicate(int[] nums){
+//        HashMap<Integer,Integer> map = new HashMap<>();
+//        for(int i = 0; i < nums.length; i++){
+//            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+//        }
+//        for(int value : map.values()){
+//            if(value != 1) return false;
+//        }
+//        return true;
+////--------------------------------------------------------------------------------------------------
+//        for(int i = 0; i < nums.length; i++){
+//            if(map.containsKey(nums[i])){
+//                return false;
+//            }
+//            map.put(nums[i],i);
+//        }
+//        return true;
+//    }
+//}
