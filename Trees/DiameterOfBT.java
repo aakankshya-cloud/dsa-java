a7a1aa2e24de66da55ff0b6f2a4ff16c3a27238f
aakankshya-cloud <aakankshyadash411@gmail.com>
2026-07-15 12:03:40 +0530

    QS

--- Trees/DiameterOfBT.java
+++ Trees/DiameterOfBT.java
@@ -1,16 +1,36 @@
+//public class DiameterOfBT {
+//    private int diameter = 0;
+//    public int diameterOfBinaryTree(TreeNode root) {
+//        HeightOfBinaryTree(root);   // Which function goes here?
+//        return diameter;
+//    }
+//    private int HeightOfBinaryTree(TreeNode root){
+//        if(root == null){
+//            return 0;
+//        }
+//        int left = diameterOfBinaryTree(root.left);
+//        int right = diameterOfBinaryTree(root.right);
+//        diameter = Math.max(diameter,left + right);
+//        return Math.max(left,right)+1;
+//    }
+//}
+
+
+
+
+
 public class DiameterOfBT {
-    private int diameter = 0;
-    public int diameterOfBinaryTree(TreeNode root) {
-        HeightOfBinaryTree(root);   // Which function goes here?
-        return diameter;
-    }
-    private int HeightOfBinaryTree(TreeNode root){
+    int diameter = 0;
+    public int height(TreeNode root){
         if(root == null){
             return 0;
         }
-        int left = diameterOfBinaryTree(root.left);
-        int right = diameterOfBinaryTree(root.right);
-        diameter = Math.max(diameter,left + right);
-        return Math.max(left,right)+1;
+        int left = height(root.left);
+        int right = height(root.right);
+        return Math.max(diameter,left + right);
+    }
+    public int diameterOfBinaryTree(TreeNode root){
+        height(root);
+        return diameter;
     }
 }
