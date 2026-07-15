afe4be9c020cfebe49bf40439f8b823ccfd1effa
aakankshya-cloud <aakankshyadash411@gmail.com>
2026-07-15 23:36:32 +0530

    QS

--- Trees/RightSideView.java
+++ Trees/RightSideView.java
@@ -1,2 +1,18 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Random;
+
 public class RightSideView {
+    public void Right(TreeNode root,int level){
+        if(root == null){
+            return;
+        }
+        ArrayList<Integer> arr = new ArrayList<>();
+        if(level == arr.size()){
+            arr.add(root.val);
+        }
+        Right(root.right,level + 1);
+        Right(root.left,level + 1);
+    }
 }
