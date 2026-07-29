4d76b8ad6ad88cd6495b4acc64e0e6cebcd74d4b
aakankshya-cloud <aakankshyadash411@gmail.com>
2026-07-29 19:22:59 +0530

    Question

--- Revision/Trees/KthElement.java
+++ Revision/Trees/KthElement.java
@@ -1,15 +1,33 @@
 import java.util.ArrayList;
+import java.util.Collections;
 
+//public class KthElement {
+//    public void inorder(TreeNode root, ArrayList<Integer> arr){
+//        if(root == null) return;
+//        inorder(root.left,arr);
+//        arr.add(root.val);
+//        inorder(root.right,arr);
+//    }
+//    public int kthSmallest(TreeNode root, int k){
+//        ArrayList<Integer> arr = new ArrayList<>();
+//        inorder(root,arr);
+//        return arr.get(k - 1);
+//    }
+//}
+
+
+// naive method
 public class KthElement {
-    public void inorder(TreeNode root, ArrayList<Integer> arr){
+    public void preorder(TreeNode root, ArrayList<Integer> arr){
         if(root == null) return;
-        inorder(root.left,arr);
         arr.add(root.val);
-        inorder(root.right,arr);
+        preorder(root.left,arr);
+        preorder(root.right,arr);
     }
     public int kthSmallest(TreeNode root, int k){
         ArrayList<Integer> arr = new ArrayList<>();
-        inorder(root,arr);
+        preorder(root,arr);
+        Collections.sort(arr);
         return arr.get(k - 1);
     }
 }
