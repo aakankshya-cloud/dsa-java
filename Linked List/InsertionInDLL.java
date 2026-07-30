96c3f8d635166c24a71456c05df0d5e45ac0cba1
aakankshya-cloud <aakankshyadash411@gmail.com>
2026-07-30 20:21:59 +0530

    Questions

--- Linked List/InsertionInDLL.java
+++ Linked List/InsertionInDLL.java
@@ -16,4 +16,64 @@ public class InsertionInDLL {
             prev = temp;
         }
     }
+    public Node InsertAtF(Node head,int data){
+        Node node = new Node(data);
+        if(head == null){
+            head = node;
+            return head;
+        }
+        node.next = head;
+        head.back = node;
+        head = node;
+        return head;
+    }
+    public Node InsertAtLast(Node head, int data){
+        Node node = new Node(data);
+        if(head == null){
+            head = node;
+        }
+        Node temp = head;
+        while(temp.next != null){
+            temp = temp.next;
+        }
+        temp.next = node;
+        node.back = temp;
+        return head;
+    }
+    public Node InsertAtK(Node head, int data, int k){
+        Node node = new Node(data);
+        if(head == null){
+            if(k == 1) {
+                head = node;
+                return head;
+            }
+            return null;
+        }
+        if(k == 1){
+            node.next = head;
+            head.back = node;
+            head = node;
+            return head;
+        }
+        Node temp = head;
+        Node prev = null;
+        int cnt = 0;
+        while(temp != null){
+            cnt++;
+            if(cnt == k){
+                prev.next = node;
+                node.back = prev;
+                node.next = temp;
+                temp.back = node;
+                return head;
+            }
+            prev = temp;
+            temp = temp.next;
+        }
+        if(cnt + 1 == k){
+            prev.next = node;
+            node.back = prev;
+        }
+        return head;
+    }
 }
