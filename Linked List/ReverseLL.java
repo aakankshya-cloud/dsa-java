c101db56c12c885fedb2a442e06459d2af1e6a96
aakankshya-cloud <aakankshyadash411@gmail.com>
2026-08-04 00:10:18 +0530

    Qs

--- Linked List/ReverseLL.java
+++ Linked List/ReverseLL.java
@@ -3,17 +3,14 @@ import java.util.LinkedList;
 
 public class ReverseLL {
     public Node Reverse(Node head){
-        Deque<Integer> stack = new LinkedList<>();
         Node temp = head;
+        Node prev = null;
         while(temp != null){
-            stack.push(temp.data);
-            temp = temp.next;
+            Node front = temp.next;
+            temp.next = prev;
+            prev = temp;
+            temp = front;
         }
-        temp = head;
-        while(temp != null){
-            temp.data = stack.pop();
-            temp = temp.next;
-        }
-        return head;
+        return prev;
     }
 }
