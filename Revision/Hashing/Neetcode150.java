f41eb6304ca9c13e3f0036507b288f820fe15448
aakankshya-cloud <aakankshyadash411@gmail.com>
2026-08-03 12:42:39 +0530

    Qs

--- Revision/Hashing/Neetcode150.java
+++ Revision/Hashing/Neetcode150.java
@@ -1,20 +1,38 @@
 import java.util.*;
 
 public class Neetcode150 {
-    public int maxArea(int[] heights){
-        int i = 0, j = heights.length - 1;
-        int max = 1;
-        int area = 1;
-        while(i < j){
-            area = Math.min(heights[i] , heights[j]) * (j - i);
-            max = Math.max(max,area);
-            if(heights[i] < heights[j]){
-                i++;
-            }
-            else{
-                j--;
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> list = new ArrayList<>();
+        Arrays.sort(nums);
+        for(int i = 0; i < nums.length - 2; i++) {
+            if(i > 0 && nums[i] == nums[i-1])
+                continue;
+            int left = i + 1 , right = nums.length - 1;
+            while(left < right){
+                int sum = nums[i] + nums[left] + nums[right];
+                if(sum > 0){
+                    right--;
+                }
+                else if(sum < 0){
+                    left++;
+                }
+                else{
+                    ArrayList<Integer> arr = new ArrayList<>();
+                    arr.add(nums[i]);
+                    arr.add(nums[left]);
+                    arr.add(nums[right]);
+                    list.add(arr);
+                    left++;
+                    right--;
+                    while(left < right && nums[left] == nums[left - 1]){
+                        left++;
+                    }
+                    while(left < right && nums[right] == nums[right + 1]){
+                        right--;
+                    }
+                }
             }
         }
-        return max;
     }
+
 }
