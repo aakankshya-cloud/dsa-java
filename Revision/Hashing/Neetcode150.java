37e3cb9d9bd64c3202bec422f3e6e35b751e2212
aakankshya-cloud <aakankshyadash411@gmail.com>
2026-08-03 19:03:20 +0530

    Qs

--- Revision/Hashing/Neetcode150.java
+++ Revision/Hashing/Neetcode150.java
@@ -1,25 +1,28 @@
 import java.util.*;
 
 public class Neetcode150 {
-    public int lengthOfLongestSubstring(String s){
-        HashSet<Character> set = new HashSet<>();
-        int i = 0, j = 0, max = -1;
-        while(j < s.length()){
-            if(!set.contains(s.charAt(j))){
-                set.add(s.charAt(j));
-            }
-            else{
-                max = Integer.max(max,set.size());
-                while(set.contains(s.charAt(j))) {
-                    set.remove(s.charAt(i));
-                    i++;
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length, n = matrix[0].length;
+        for(int i = 0; i < m; i++){
+            if(matrix[i][0] <= target && matrix[i][n - 1] >= target){
+                int left = 0;
+                int right = n - 1;
+                while(left <= right){
+                    int mid = (left + right) / 2;
+                    if(target == matrix[i][mid]){
+                        return true;
+                    }
+                    else if(target > matrix[i][mid]){
+                        left = mid + 1;
+                    }
+                    else{
+                        right = mid - 1;
+                    }
                 }
-                set.add(s.charAt(j));
+                return false;
             }
-            j++;
-            max = Integer.max(max,set.size());
         }
-        return max;
+        return false;
 
     }
 }
