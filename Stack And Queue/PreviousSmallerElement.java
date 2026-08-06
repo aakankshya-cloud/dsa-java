447a96adc44ae3c136798ec78517c460ba167080
aakankshya-cloud <aakankshyadash411@gmail.com>
2026-08-06 20:26:48 +0530

    Previous Smaller Element

---
+++ Stack And Queue/PreviousSmallerElement.java
@@ -0,0 +1,16 @@
+import java.util.Stack;
+
+public class PreviousSmallerElement {
+    public int[] prevSmallerElements(int[] nums){
+        int[] result = new int[nums.length];
+        Stack<Integer> stack = new Stack<>();
+        for(int i = 0; i < nums.length; i++) {
+            while (!stack.isEmpty() && stack.peek() >= nums[i]){
+                stack.pop();
+            }
+            result[i] = stack.isEmpty() ? -1 : stack.peek();
+            stack.push(nums[i]);
+        }
+        return result;
+    }
+}
