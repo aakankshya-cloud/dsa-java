aaaa63927923793c6ce181dd05fac9c7ecc8326d
aakankshya-cloud <aakankshyadash411@gmail.com>
2026-08-07 11:45:19 +0530

    Sum OF minimum of subarray

--- Stack And Queue/SumOfSubarrayMin.java
+++ Stack And Queue/SumOfSubarrayMin.java
@@ -5,9 +5,22 @@ public class SumOfSubarrayMin  {
             int min = Integer.MAX_VALUE;
             for(int j = i ; j < arr.length; j++){
                 min = Math.min(arr[j], min);
-                sum = sum + min;
+                sum = (sum + min) ;
             }
         }
         return sum;
+    }
+
+
+    public int Sum(int[] arr){
+        int total = 0;
+        int[] next = findNextSmaller(arr);
+        int[] prev = findPrevSmaller(arr);
+        for(int i = 0; i < arr.length - 1; i++){
+            int left = i - prev[i];
+            int right = next[i] - i;
+            total += (right * left * arr[i]);
+        }
+        return total;
     }
 }
