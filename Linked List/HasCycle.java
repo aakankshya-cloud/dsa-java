cc0a2cc3f3080c751ae0933b9c60c17e54fc1e2d
aakankshya-cloud <aakankshyadash411@gmail.com>
2026-08-10 13:08:17 +0530

    Detect Cycle

--- Linked List/HasCycle.java
+++ Linked List/HasCycle.java
@@ -1,5 +1,27 @@
-public class HasCycle {
-    public boolean hasCycle(ListNode head){
+import java.util.HashSet;
 
+public class HasCycle {
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+    }
+    public boolean hasCycle(ListNode head){
+        ListNode temp = head;
+        HashSet<ListNode> set = new HashSet<>();
+        while(temp != null){
+            if(set.contains(temp)){
+                return true;
+            }
+            set.add(temp);
+            temp = temp.next;
+        }
+        return false;
     }
 }
