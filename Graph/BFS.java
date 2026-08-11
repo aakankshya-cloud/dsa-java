2bf1bca3c94fb7eb22dd909b3389d03487f61066
aakankshya-cloud <aakankshyadash411@gmail.com>
2026-08-11 19:38:44 +0530

    BFS

--- Graph/BFS.java
+++ Graph/BFS.java
@@ -5,16 +5,16 @@ import java.util.Queue;
 public class BFS {
     public ArrayList<Integer> BFSGraph(int V, ArrayList<ArrayList<Integer>> adj){
         ArrayList<Integer> bfs = new ArrayList<>();
-        boolean visited[] = new boolean[V];
+        boolean[] vis = new boolean[V];
         Queue<Integer> queue = new LinkedList<>();
         queue.add(0);
-        visited[0] = true;
-        while(!queue.isEmpty()) {
-            Integer node = queue.poll();
+        vis[0] = true;
+        while(!queue.isEmpty()){
+            int node = queue.poll();
             bfs.add(node);
-            for (Integer it : adj.get(node)) {
-                if (!visited[it]) {
-                    visited[it] = true;
+            for(int it : adj.get(node)){
+                if(vis[it] == false){
+                    vis[it] = true;
                     queue.add(it);
                 }
             }
