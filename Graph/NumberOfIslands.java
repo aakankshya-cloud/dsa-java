bfb77102c150359742ece639a566a5112322b7ba
aakankshya-cloud <aakankshyadash411@gmail.com>
2026-08-14 23:52:30 +0530

    Max Length Substring With Two Occurences

--- Graph/NumberOfIslands.java
+++ Graph/NumberOfIslands.java
@@ -1,26 +1,20 @@
+import java.util.ArrayList;
+
 public class NumberOfIslands {
     public int numIslands(char[][] grid){
-        int count = 0;
-        int rows = grid.length;
-        int cols = grid[0].length;
-        for(int i = 0; i < rows; i++){
-            for(int j = 0; j < cols; j++){
-                if(grid[i][j] == 1){
-                    count++;
-                    dfs(grid,i,j);
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<ArrayList<Integer>>();
+        for(int i = 0; i < grid.length; i++){
+            for(int j = 0; j < grid[0].length; j++){
+                if(grid[i][j] && i != j){
+                    adj.get(i).add(j);
+                    adj.get(j).add(i);
                 }
             }
         }
-        return count;
+
     }
     private void dfs(char[][] grid, int row, int col){
-        if(row < 0 || col < 0 || row >= grid.length || col >= grid[0].length || grid[row][col] == '0'){
-            return;
-        }
-        grid[row][col] = '0';
-        dfs(grid,row+1,col);
-        dfs(grid,row - 1, col);
-        dfs(grid,row,col+1);
-        dfs(grid,row,col - 1);
+
+
     }
 }
