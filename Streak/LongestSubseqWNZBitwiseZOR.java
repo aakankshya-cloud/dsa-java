45ad566b4a2af6d74cefd386bde7524f3ff7af19
aakankshya-cloud <aakankshyadash411@gmail.com>
2026-08-15 19:13:14 +0530

    Longest Bitwise Subseq With NZ Bitwise XOR

---
+++ Streak/LongestSubseqWNZBitwiseZOR.java
@@ -0,0 +1,19 @@
+public class LongestSubseqWNZBitwiseZOR {
+    public int longestSubsequence(int[] nums){
+        int xor = 0;
+        boolean hasNonZero = false;
+        for(int num : nums){
+            xor = xor ^ num;
+            if(num != 0){
+                hasNonZero = true;
+            }
+        }
+        if(xor != 0){
+            return nums.length;
+        }
+        if(hasNonZero){
+            return nums.length - 1;
+        }
+        return 0;
+    }
+}
