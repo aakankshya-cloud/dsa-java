28e30f6c71f0d41d09c55a28e09423da49f6e864
aakankshya-cloud <aakankshyadash411@gmail.com>
2026-08-15 19:40:36 +0530

    Trapping RainWater

---
+++ Revision/Revision/TrappingRWater.java
@@ -0,0 +1,24 @@
+package Revision.Revision;
+
+public class TrappingRWater {
+    public int trap(int[] height){
+        int[] prefixMax = new int[height.length];
+        int[] suffixMax = new int[height.length];
+        prefixMax[0] = height[0];
+        suffixMax[height.length - 1] = height[height.length - 1];
+        for(int i = 1; i < height.length; i++){
+            prefixMax[i] = Math.max(height[i],prefixMax[i - 1]);
+        }
+        for(int i = height.length - 2; i >= 0; i--){
+            suffixMax[i] = Math.max(suffixMax[i + 1],height[i]);
+        }
+        int total = 0;
+        for(int i = 0; i < height.length; i++){
+            int leftMax = prefixMax[i], rightMax = suffixMax[i];
+            if(height[i] < prefixMax[i] && height[i] < suffixMax[i]){
+                total = total + (Math.min(leftMax,rightMax) - height[i]);
+            }
+        }
+        return total;
+    }
+}
