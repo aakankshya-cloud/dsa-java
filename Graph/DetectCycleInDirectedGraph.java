f71443008fc93004bcc918c338bf6ebe7ec86cda
aakankshya-cloud <aakankshyadash411@gmail.com>
2026-08-19 19:55:05 +0530

    Detect cycle in directed graph

--- Graph/DetectCycleInDirectedGraph.java
+++ Graph/DetectCycleInDirectedGraph.java
@@ -1,21 +1,29 @@
 import java.util.ArrayList;
 
 public class DetectCycleInDirectedGraph {
-    public boolean checkForCycle(ArrayList<ArrayList<Integer>> adj, boolean[] vis, int src, int V){
+    public boolean checkForCycle(ArrayList<ArrayList<Integer>> adj, boolean[] vis, int src, int V, boolean[] pathVis){
         vis[src] = true;
         for(int adjNode : adj.get(src)){
             if(!vis[adjNode]) {
-                checkForCycle(adj, vis, adjNode, V);
+                if(checkForCycle(adj, vis, adjNode, V, pathVis)) return true;
             }
             else{
-                if(adjNode == )
+                if(pathVis[adjNode]){
+                    return true;
+                }
             }
         }
-
-
+        pathVis[src] = false;
+        return false;
     }
     public boolean isCycle(int V, ArrayList<ArrayList<Integer>> adj){
         boolean[] vis = new boolean[V];
-
+        boolean[] pathVis = new boolean[V];
+        for(int i = 0; i < V; i++){
+            if(!vis[i]){
+                if(checkForCycle(adj,vis,i,V,pathVis)) return true;
+            }
+        }
+        return false;
     }
 }
