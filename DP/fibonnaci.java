4938edd4642035f55f985fa5a1891cd0d17fbbe7
aakankshya-cloud <aakankshyadash411@gmail.com>
2026-08-24 15:07:12 +0530

    Fibonacci

---
+++ DP/fibonnaci.java
@@ -0,0 +1,20 @@
+package DP;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class fibonnaci {
+    public static int fibo(int n, int[] dp){
+        if(n <= 1) return n;
+        if(dp[n] != -1){
+            return dp[n];
+        }
+        return dp[n] = fibo(n - 1, dp) + fibo(n - 2, dp);
+    }
+    public static void main(String[] args) {
+        int n = 5;
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp,-1);
+        System.out.println(fibo(n,dp));
+    }
+}
