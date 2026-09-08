f54f40284bd536be038c2aebcd4651057ce28276
aakankshya-cloud <aakankshyadash411@gmail.com>
2026-09-09 00:24:21 +0530

    DSA REVISION

---
+++ rev/KadanesAlgo.java
@@ -0,0 +1,14 @@
+public class KadanesAlgo {
+    public int maxSubArray(int[] nums){
+        int sum = 0;
+        int maxSum = nums[0];
+        for(int num : nums){
+            sum += num;
+            maxSum = Math.max(maxSum, sum);
+            if(sum < 0){
+                sum = 0;
+            }
+        }
+        return maxSum;
+    }
+}
