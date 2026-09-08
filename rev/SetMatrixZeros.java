a30a3c2712e42cf768a447d50b731a0058fa02f3
aakankshya-cloud <aakankshyadash411@gmail.com>
2026-09-08 23:57:03 +0530

    DSA REVISION

--- rev/SetMatrixZeros.java
+++ rev/SetMatrixZeros.java
@@ -15,7 +15,7 @@ public class SetMatrixZeros {
         for(int i = 0; i < m; i++){
             for(int j = 0; j < n; j++){
                 if(row[i] == 1 || col[j] == 1){
-                    matrix[i][j] = 0;
+                    matrix[i][j] = 1;
                 }
             }
         }
